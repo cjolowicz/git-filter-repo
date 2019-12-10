e70ee60d5500e6ba060969a491afefec3743969e
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-12-10 09:31:22 +0100

    Move _GitElementWithId to separate module

--- src/git_filter_repo/console.py
+++ src/git_filter_repo/console.py
@@ -13,7 +13,7 @@ import time
 import textwrap
 
 from .ancestrygraph import AncestryGraph
-from .elements import _GitElement
+from .elements import _GitElement, _GitElementWithId
 from .gettext import _, setup_gettext
 from .ids import _IDS
 from .mailmap import MailmapInfo
@@ -52,31 +52,6 @@ def glob_to_regex(glob_bytestr):
 
     # Finally, convert back to regex operating on bytestr
     return regex.encode()
-
-
-class _GitElementWithId(_GitElement):
-    """
-    The base class for Git elements that have IDs (commits and blobs)
-    """
-
-    def __init__(self):
-        _GitElement.__init__(self)
-
-        # The mark (short, portable id) for this element
-        self.id = _IDS.new()
-
-        # The previous mark for this element
-        self.old_id = None
-
-    def skip(self, new_id=None):
-        """
-        This element will no longer be automatically written to output. When a
-        commit gets skipped, it's ID will need to be translated to that of its
-        parent.
-        """
-        self.dumped = 2
-
-        _IDS.record_rename(self.old_id or self.id, new_id)
 
 
 class Blob(_GitElementWithId):
--- src/git_filter_repo/elements.py
+++ src/git_filter_repo/elements.py
@@ -1,5 +1,7 @@
 import io
 
+from .ids import _IDS
+
 
 class _GitElement(object):
     """
@@ -45,3 +47,26 @@ class _GitElement(object):
         self.dumped = 2
 
 
+class _GitElementWithId(_GitElement):
+    """
+    The base class for Git elements that have IDs (commits and blobs)
+    """
+
+    def __init__(self):
+        _GitElement.__init__(self)
+
+        # The mark (short, portable id) for this element
+        self.id = _IDS.new()
+
+        # The previous mark for this element
+        self.old_id = None
+
+    def skip(self, new_id=None):
+        """
+        This element will no longer be automatically written to output. When a
+        commit gets skipped, it's ID will need to be translated to that of its
+        parent.
+        """
+        self.dumped = 2
+
+        _IDS.record_rename(self.old_id or self.id, new_id)
