7309ac26f2bd828f76576d864c320332ba568983
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-12-10 09:30:07 +0100

    Move _GitElement to separate module

--- src/git_filter_repo/console.py
+++ src/git_filter_repo/console.py
@@ -13,6 +13,7 @@ import time
 import textwrap
 
 from .ancestrygraph import AncestryGraph
+from .elements import _GitElement
 from .gettext import _, setup_gettext
 from .ids import _IDS
 from .mailmap import MailmapInfo
@@ -51,50 +52,6 @@ def glob_to_regex(glob_bytestr):
 
     # Finally, convert back to regex operating on bytestr
     return regex.encode()
-
-
-class _GitElement(object):
-    """
-    The base class for all git elements that we create.
-    """
-
-    def __init__(self):
-        # A string that describes what type of Git element this is
-        self.type = None
-
-        # A flag telling us if this Git element has been dumped
-        # (i.e. printed) or skipped.  Typically elements that have been
-        # dumped or skipped will not be dumped again.
-        self.dumped = 0
-
-    def dump(self, file_):
-        """
-        This version should never be called. Derived classes need to
-        override! We should note that subclasses should implement this
-        method such that the output would match the format produced by
-        fast-export.
-        """
-        raise SystemExit(
-            _("Unimplemented function: %s") % type(self).__name__ + ".dump()"
-        )  # pragma: no cover
-
-    def __bytes__(self):
-        """
-        Convert GitElement to bytestring; used for debugging
-        """
-        old_dumped = self.dumped
-        writeme = io.BytesIO()
-        self.dump(writeme)
-        output_lines = writeme.getvalue().splitlines()
-        writeme.close()
-        self.dumped = old_dumped
-        return b"%s:\n  %s" % (type(self).__name__.encode(), b"\n  ".join(output_lines))
-
-    def skip(self, new_id=None):
-        """
-        Ensures this element will not be written to output
-        """
-        self.dumped = 2
 
 
 class _GitElementWithId(_GitElement):
